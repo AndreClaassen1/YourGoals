0bfec5cdfb89e474a3839b80391d7d3137c8332f
Chaosspeeder <claassenandre@googlemail.com>
2017-10-22 11:57:15 +0200

    Initial Commit

---
+++ YourGoals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YourGoals
+//
+//  Created by André Claaßen on 22.10.17.
+//  Copyright © 2017 André Claaßen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
