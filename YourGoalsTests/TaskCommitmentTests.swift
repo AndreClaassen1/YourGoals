b71fa631a3c8ceac3968a11a4657a610e011bf45
Chaosspeeder <claassenandre@googlemail.com>
2017-11-01 15:26:19 +0100

    all tests green.

--- YourGoalsTests/TaskCommitmentTests.swift
+++ YourGoalsTests/TaskCommitmentTests.swift
@@ -22,7 +22,26 @@ class TaskCommitmentTests: StorageTestCase {
         
         // done
         XCTAssertEqual(CommittingState.notCommitted, task.commitingState(forDate: testDate))
-        
     }
+    
+    func testCommitmentForDate() {
+        // setup
+        let testDate = Date.dateWithYear(2017, month: 11, day: 1)
+        let factory = TaskFactory(manager: self.manager)
+        let task = factory.create(name: "Task", state: .active, prio: 1)
+        
+        // act
+        let commitmentManager = TaskCommitmentManager(manager: self.manager)
+        try! commitmentManager.commit(task: task, forDate: testDate)
+        
+        // done
+        let committedTasks = try! commitmentManager.committedTasks(forDate: testDate)
+        XCTAssertEqual(1, committedTasks.count)
+        XCTAssertEqual(CommittingState.committedForDate, committedTasks[0].commitingState(forDate: testDate))
+    }
+    
+    
+    
+    
     
 }
