5d19aac6b2c485b3d5e89b99d54a85a50398307a
Chaosspeeder <claassenandre@googlemail.com>
2017-11-06 19:00:08 +0100

    simple unit test for unchecking an habit.

--- YourGoalsTests/HabitManagerTests.swift
+++ YourGoalsTests/HabitManagerTests.swift
@@ -24,4 +24,19 @@ class HabitManagerTests: StorageManagerWorker {
         let habitReloaded = self.manager.context.object(with: habit.objectID) as! Habit
         XCTAssert(habitReloaded.isChecked(forDate: date))
     }
+    
+    func testUnCheckHabitForDate() {
+        // setup
+        let dateTime = Date.dateTimeWithYear(2017, month: 11, day: 06, hour: 12, minute: 13, second: 0)
+        let date = Date.dateWithYear(2017, month: 11, day: 06)
+        let habit = HabitFactory(manager: self.manager).createHabit(name: "Test Habit")
+        
+        // act
+        try! HabitManager(manager: self.manager).checkHabit(forHabit: habit, state: .checked , atDate: dateTime)
+        try! HabitManager(manager: self.manager).checkHabit(forHabit: habit, state: .notChecked , atDate: dateTime)
+
+        // test
+        let habitReloaded = self.manager.context.object(with: habit.objectID) as! Habit
+        XCTAssertFalse(habitReloaded.isChecked(forDate: date))
+    }
 }
