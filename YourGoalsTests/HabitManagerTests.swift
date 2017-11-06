2641110f7ae90eb409694905a8362eb7c00c590e
Chaosspeeder <claassenandre@googlemail.com>
2017-11-06 18:55:31 +0100

    now I can check or uncheck an habit.

--- YourGoalsTests/HabitManagerTests.swift
+++ YourGoalsTests/HabitManagerTests.swift
@@ -9,15 +9,19 @@
 import XCTest
 @testable import YourGoals
 
-class HabitManagerTests: XCTestCase {
+class HabitManagerTests: StorageManagerWorker {
     
     func testCheckHabitForDate() {
         // setup
+        let dateTime = Date.dateTimeWithYear(2017, month: 11, day: 06, hour: 12, minute: 13, second: 0)
+        let date = Date.dateWithYear(2017, month: 11, day: 06)
+        let habit = HabitFactory(manager: self.manager).createHabit(name: "Test Habit")
         
         // act
+        try! HabitManager(manager: self.manager).checkHabit(forHabit: habit, state: .checked , atDate: dateTime)
         
         // test
-        
+        let habitReloaded = self.manager.context.object(with: habit.objectID) as! Habit
+        XCTAssert(habitReloaded.isChecked(forDate: date))
     }
-    
 }
