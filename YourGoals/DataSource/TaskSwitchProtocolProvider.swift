0b1ac8aeeed4685e4800b734875db0c14db1fe95
Chaosspeeder <claassenandre@googlemail.com>
2017-11-22 22:22:15 +0100

    some code comments.

--- YourGoals/DataSource/TaskSwitchProtocolProvider.swift
+++ YourGoals/DataSource/TaskSwitchProtocolProvider.swift
@@ -14,12 +14,19 @@ class TaskSwitchProtocolProvider {
     let progressManager:ActionableSwitchProtocol
     let commitmentManager:ActionableSwitchProtocol
     
+    /// init the protocol provider with the core data storage manager
+    ///
+    /// - Parameter manager: a storage manager
     init(manager:GoalsStorageManager) {
         self.stateManager = TaskStateManager(manager: manager)
         self.progressManager = TaskProgressManager(manager: manager)
         self.commitmentManager = TaskCommitmentManager(manager: manager)
     }
     
+    /// get the task swith protocol for the desired behaviour (like, completion, starting a work or checking a habt)
+    ///
+    /// - Parameter behavior: the behaviour like state of the task, commitment for work or start working (progress)
+    /// - Returns: an abstract switching protocol for the desired behavior
     func switchProtocol(forBehavior behavior: ActionableBehavior) -> ActionableSwitchProtocol {
         switch behavior {
         case .commitment:
