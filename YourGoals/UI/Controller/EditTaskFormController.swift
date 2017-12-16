367c79f98f00c5f3757ba8458ff173cdca9740b9
Chaosspeeder <claassenandre@googlemail.com>
2017-12-16 08:06:35 +0100

    first working form

--- YourGoals/UI/Controller/EditTaskFormController.swift
+++ YourGoals/UI/Controller/EditTaskFormController.swift
@@ -37,6 +37,8 @@ class EditTaskFormController : FormViewController, EditActionableViewControllerP
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.leftBarButtonItem?.target = self
         self.navigationItem.leftBarButtonItem?.action = #selector(cancelTapped(_:))
+        self.navigationItem.rightBarButtonItem?.target = self
+        self.navigationItem.rightBarButtonItem?.action = #selector(saveTapped(_:))
         
         do {
             let viewModel = try EditTaskForm(manager: self.manager, goal: self.goal, actionable: self.editActionable)
@@ -46,18 +48,16 @@ class EditTaskFormController : FormViewController, EditActionableViewControllerP
             self.showNotification(forError: error)
         }
     }
-    
-    @objc func cancelTapped(_ barButtonItem: UIBarButtonItem) {
-        self.dismiss(animated: false)
-    }
-    
+ 
     private func createForm(withViewModel viewModel:EditTaskForm) {
         assert(self.parameterCommitted, "The parameter weren't committed")
-        form +++
-            Section()
+        form
+            +++ Section()
             <<< TextRow(tag: EditTaskFormTag.taskTag.rawValue).cellSetup { cell, row in
                 cell.textField.placeholder = row.tag
                 row.value = viewModel.item(tag: row.tag).value
+                row.add(rule: RuleRequired())
+                row.validationOptions = .validatesAlways
             }
             
             <<< PushRow<Goal>(EditTaskFormTag.goalTag.rawValue) { row in
@@ -72,9 +72,9 @@ class EditTaskFormController : FormViewController, EditActionableViewControllerP
                 }.cellUpdate{ (cell, row) in
                     cell.textLabel?.text = "Goal"
                     cell.detailTextLabel?.text = row.value?.name
-                }
-            Section()
-                <<< PushRow<String>() { row in
+            }
+            +++ Section()
+            <<< PushRow<String>() { row in
                 row.tag = EditTaskFormTag.commitDateTag.rawValue
                 row.title = "Select a commit date"
                 row.options = ["Today", "tomorrow", "Tuesday"]
@@ -83,13 +83,58 @@ class EditTaskFormController : FormViewController, EditActionableViewControllerP
                 ///
                 ///     Uncommit, Today, Tomorrow, Di, Mi,Do, Fr, Sa, Son
                 ///     Schedule
-                }
-            Section()
-                <<< TextAreaRow() {
-                        $0.placeholder = "Remarks on your task"
-                        $0.textAreaHeight = .dynamic(initialTextViewHeight: 110)
-                }
+            }
+            +++ Section()
+            <<< TextAreaRow() {
+                $0.placeholder = "Remarks on your task"
+                $0.textAreaHeight = .dynamic(initialTextViewHeight: 110)
+        }
     }
+    
+    // MARK: Navigation Bar Button Events
+    
+    @objc func cancelTapped(_ barButtonItem: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    @objc func saveTapped(_ barButtonItem: UIBarButtonItem) {
+        do {
+            
+            let validationErrors = form.validate()
+            guard validationErrors.count == 0 else {
+                showNotification(text: "There are validation errors")
+                return
+            }
+            
+            guard let actionableInfo = actionableInfoFromFields() else {
+                showNotification(text: "Bug: Could not create a task or habit")
+                return
+            }
+            
+            if let actionable = self.editActionable  {
+                try delegate?.updateActionable(actionable: actionable, updateInfo: actionableInfo)
+            } else {
+                try delegate?.createNewActionable(actionableInfo: actionableInfo)
+            }
+            
+            self.dismiss(animated: true)
+        }
+        catch let error {
+            NSLog("could not create a new task info from fields. \(error.localizedDescription)")
+            self.showNotification(forError: error)
+        }
+    }
+    
+    func actionableInfoFromFields() -> ActionableInfo? {
+        let row:TextRow = self.form.rowBy(tag: EditTaskFormTag.taskTag.rawValue)!
+        guard let name = row.value else {
+            NSLog("couldn't read name of task or habit")
+            return nil
+        }
+        
+        return ActionableInfo(type: .task, name: name)
+    }
+    
     
     // Mark: - data helper methods
 }
